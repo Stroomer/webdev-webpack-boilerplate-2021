1689a93b99dcb30cb557899161274e8871cde339
Lukas <lstroomer@glu.nl>
2021-09-03 15:50:56 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("test!");
+console.log("test complete!");
+
+const container = document.getElementById("container");
+const div = document.createElement("div");
+
+div.innerHTML = "this boilerplate is ready for action!";
+div.style.fontSize = "48px";
+div.style.color = "red";
+div.style.backgroundColor = "black";
+
+container.appendChild(div);
